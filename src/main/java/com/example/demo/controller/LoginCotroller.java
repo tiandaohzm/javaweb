30722346bc96d0adac5f9587a8cd85438ae6e09f
hzm <773199177@qq.com>
2019-08-21 08:46:22 +0800

    初次提交

---
+++ src/main/java/com/example/demo/controller/LoginCotroller.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+/**
+ * @decription 登录控制器，负责用户登录，注册
+ */
+@Controller
+public class LoginCotroller {
+    @GetMapping("login")
+    public String index(){
+        return "login";
+    }
+    @PostMapping("login")
+    public void login(String username,String password){
+        Subject subject=SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        token.setRememberMe(true);
+        try {
+            subject.login(token);
+        }catch (IncorrectCredentialsException e){
+            e.printStackTrace();
+        }catch (UnknownAccountException e){
+            e.printStackTrace();
+        }
+    }
+}
